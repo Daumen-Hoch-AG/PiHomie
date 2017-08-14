9efcd10082eae5a8b331651cc7e44eeb2080590c
Pitastic <mickep1985@gmail.com>
2017-08-14 12:44:09 +0200

    Konzeptzeichnung

---
+++ docs/Steuerung.mm
@@ -0,0 +1,85 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1502705234593" ID="ID_1372833683" MODIFIED="1502705244363" TEXT="Steuerung">
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502705288012" HGAP="31" ID="ID_554064687" MODIFIED="1502706591981" POSITION="right" STYLE="bubble" TEXT="Bottle" VSHIFT="-4">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1502705728593" HGAP="18" ID="ID_475778248" MODIFIED="1502706066786" STYLE="bubble" TEXT="PHP" VSHIFT="2">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node BACKGROUND_COLOR="#58a7d5" COLOR="#010101" CREATED="1502705768569" ID="ID_1001319158" MODIFIED="1502705948279" STYLE="bubble" TEXT="UI">
+<edge COLOR="#0d6e9e" STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#58a7d5" COLOR="#010101" CREATED="1502705449047" HGAP="22" ID="ID_791068492" MODIFIED="1502706789799" POSITION="left" STYLE="bubble" TEXT="Datenbank" VSHIFT="-54">
+<edge COLOR="#0d6e9e" STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="xmag"/>
+</node>
+<node CREATED="1502705451743" HGAP="35" ID="ID_884137512" MODIFIED="1502706786543" POSITION="left" STYLE="bubble" TEXT="Sys-Log" VSHIFT="-15">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502705460903" HGAP="-17" ID="ID_552316354" MODIFIED="1502706600349" POSITION="right" STYLE="bubble" TEXT="Bottle" VSHIFT="84">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502705460903" HGAP="49" ID="ID_669249639" MODIFIED="1502706854671" STYLE="bubble" TEXT="Bottle" VSHIFT="-62">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node BACKGROUND_COLOR="#58a7d5" COLOR="#010101" CREATED="1502706092403" HGAP="22" ID="ID_375461351" MODIFIED="1502706682340" STYLE="bubble" TEXT="Knode" VSHIFT="2">
+<edge COLOR="#0d6e9e" STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502706462254" HGAP="25" ID="ID_992783364" MODIFIED="1502706780727" STYLE="bubble" TEXT="Knode-Log" VSHIFT="-8">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="list"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502706693263" HGAP="16" ID="ID_1695767108" MODIFIED="1502706734118" STYLE="bubble" TEXT="WLAN" VSHIFT="53">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<node CREATED="1502706718615" HGAP="25" ID="ID_236773771" MODIFIED="1502706762902" TEXT="Sensor/Aktor" VSHIFT="2">
+<icon BUILTIN="clock"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502706698311" ID="ID_286962724" MODIFIED="1502706702524" STYLE="bubble" TEXT="i2c">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<node CREATED="1502706718615" HGAP="25" ID="ID_628654934" MODIFIED="1502706766823" TEXT="Sensor/Aktor" VSHIFT="2">
+<icon BUILTIN="clock"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502706704407" ID="ID_480396139" MODIFIED="1502706707795" STYLE="bubble" TEXT="spi">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<node CREATED="1502706718615" HGAP="45" ID="ID_956723568" MODIFIED="1502706771847" TEXT="Sensor/Aktor" VSHIFT="2">
+<icon BUILTIN="clock"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502705460903" HGAP="23" ID="ID_875946483" MODIFIED="1502706061242" STYLE="bubble" TEXT="Bottle" VSHIFT="2">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node BACKGROUND_COLOR="#58a7d5" COLOR="#010101" CREATED="1502706092403" HGAP="22" ID="ID_532140065" MODIFIED="1502706848287" STYLE="bubble" TEXT="Knode">
+<edge COLOR="#0d6e9e" STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1502705460903" HGAP="23" ID="ID_49311175" MODIFIED="1502706061242" STYLE="bubble" TEXT="Bottle" VSHIFT="2">
+<edge COLOR="#080606" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Verdana" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node BACKGROUND_COLOR="#58a7d5" COLOR="#010101" CREATED="1502706092403" HGAP="22" ID="ID_544966912" MODIFIED="1502706844717" STYLE="bubble" TEXT="..." VSHIFT="2">
+<edge COLOR="#0d6e9e" STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+</map>
