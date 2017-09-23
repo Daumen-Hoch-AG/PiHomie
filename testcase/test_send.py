879e4a54ab05c34099d49e1a28b32c240c3f4c4d
Pitastic <mickep1985@gmail.com>
2017-09-23 12:48:16 +0200

    Neue Ueberarbeitung der Serverfunktionalitaet mit Features
    Auf ESP noch nicht komplett getestet (nur lokal am Rechner)
    
    Pairing hat noch keine Auswirkungen - Jeder darf anweisen

---
+++ testcase/test_send.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+import hashlib
+import time
+from binascii import hexlify
+
+SALT = ""
+toSend = ""
+params = b"%%%192.168.178.X%%%1"
+
+def hashToSend():
+	global toSend
+	toSend = hashlib.sha256(str.encode("pair"+SALT)).digest()
+	toSend = hexlify(toSend)+params
+	print(toSend)
+
+def main():
+	host = ("localhost", 8080)
+	s = socket.socket()
+	try:
+		s.connect(host)
+	except Exception:
+		print("Unable to connect ! - ABORT")
+		return
+	print("Connected to {} : {}".format(host[0], host[1]))
+	print("Sending:")
+	print(toSend)
+	s.send(toSend)
+	time.sleep(10)
+	return
+
+
+if __name__ == "__main__":
+	hashToSend()
+	main()
