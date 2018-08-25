6eced736b426819f52206a6d0390860a1a90391c
Pitastic <mickep1985@gmail.com>
2018-08-25 17:40:05 +0200

    erweitertes Installskript
    Variable Config hinzugefügt

--- controller/install_core.sh
+++ controller/install_core.sh
@@ -18,6 +18,8 @@ echo "- Core"
 
 # Zusätzliche Konfigurationsmodule können hier eingehängt werden:
 modules=()
+SCRIPT_PATH="`dirname \"$0\"`"
+ROOT_PATH="`( cd \"../$SCRIPT_PATH\" && pwd )`"
 
 
 # =============================
@@ -28,16 +30,35 @@ echo
 echo "=> Starte Modul Core"
 echo
 
-addgroup pihomie
-adduser --no-create-home --disabled-login --ingroup pihomie --gecos "" pihomie
-
-git clone https://github.com/Daumen-Hoch-AG/PiHomie.git
-chown pihomie:pihomie -R /opt/PiHomie
-
+# Pakete
 apt update && apt upgrade -y
 apt install -y \
-	git
-	python3-pip
+	python3-flask \
+	apache2 libapache2-mod-wsgi-py3
+apt autoremove -y
+
+
+# Benutzer und Rechte für PiHomie
+addgroup pihomie
+adduser --no-create-home --disabled-login --ingroup pihomie --gecos "" pihomie
+usermod -a -G www-data pihomie
+chown pihomie:www-data -R $ROOT_PATH
+cd $ROOT_PATH
+
+
+# Konfigurationen
+
+# -- Hostname
+hostname -b PiHomie
+sed -i -e '/127\.0\.1\.1/ s/.*/127\.0\.1\.1\tPiHomie/g' /etc/hosts
+
+# -- Apache2
+a2enmod wsgi
+sed -i -e "s|%%PIHOMIE_ROOT%%|${ROOT_PATH}|" $SCRIPT_PATH/apache_vhost.conf
+mv $SCRIPT_PATH/apache_vhost.conf /etc/apache2/sites-available/pihomie.conf
+a2ensite pihomie
+systemctl reload apache2
+
 
 # =============================
 # Start der zusätzlichen Module:
