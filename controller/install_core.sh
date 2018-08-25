66487e32f27192d6f6781f7c2de9238e2192bf96
Pitastic <mickep1985@gmail.com>
2018-08-25 19:12:31 +0200

    Install Skript
    Bugfix

--- controller/install_core.sh
+++ controller/install_core.sh
@@ -55,7 +55,7 @@ sed -i -e '/127\.0\.1\.1/ s/.*/127\.0\.1\.1\tPiHomie/g' /etc/hosts
 
 # -- Apache2
 a2enmod wsgi
-sed -i -e "s|%%PIHOMIE_ROOT%%|${ROOT_PATH}|" $SCRIPT_PATH/apache_vhost.conf
+sed -i -e "s|%%PIHOMIE_ROOT%%|${SCRIPT_PATH}|" $SCRIPT_PATH/apache_vhost.conf
 sed -i -e "s|%%PIHOMIE_GATEWAY%%|${SCRIPT_PATH}/gateway.wsgi|" $SCRIPT_PATH/apache_vhost.conf
 mv $SCRIPT_PATH/apache_vhost.conf /etc/apache2/sites-available/pihomie.conf
 a2ensite pihomie
