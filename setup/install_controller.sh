f0cb7c6927a2a44e18e96dd5b9bddf2cba0d6579
Pitastic <mickep1985@gmail.com>
2018-08-26 12:50:19 +0200

    Installskript organisation und Variablen

--- setup/install_controller.sh
+++ setup/install_controller.sh
@@ -9,18 +9,47 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
-echo
-echo "=== PiHomie Basisinstalltion ==="
-echo
-
-echo "Module:"
-echo "- Core"
-
 # Zusätzliche Konfigurationsmodule können hier eingehängt werden:
 modules=()
+
 SCRIPT_PATH="`dirname \"$0\"`"
 SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"
 ROOT_PATH="`( cd \"$SCRIPT_PATH/..\" && pwd )`"
+HOSTNAME="PiHomie"
+
+
+echo
+echo "================================"
+echo "====== PiHomie Installtion ====="
+echo "================================"
+echo "           Controller"
+echo "--------------------------------"
+echo
+echo "Module:"
+echo "- Core (Apache2, Python 3 Frameworks)"
+
+for m in "${modules[@]}"
+do
+	echo "- $m"
+done
+
+
+# =============================
+# Benutzervariablen
+# =============================
+
+echo
+echo "> Gib den Hostname für diesen Controller ein oder lasse ihn leer für 'PiHomie' - [ENTER]"
+read user_hostname
+if [ !-z "$user_hostname" ]; then
+	HOSTNAME=$user_hostname
+fi
+
+echo $SCRIPT_PATH
+echo $ROOT_PATH
+echo $HOSTNAME
+sed -e '/127\.0\.1\.1/ s/.*/127\.0\.1\.1\t${HOSTNAME}/g' /etc/hosts
+exit 0
 
 
 # =============================
@@ -36,7 +65,7 @@ apt update && apt upgrade -y
 apt install -y \
 	python3-flask python3-requests \
 	apache2 libapache2-mod-wsgi-py3
-apt autoremove -y
+#apt autoremove -y
 
 
 # Benutzer und Rechte für PiHomie
@@ -50,14 +79,14 @@ cd $ROOT_PATH
 # Konfigurationen
 
 # -- Hostname
-hostname -b PiHomie
-sed -i -e '/127\.0\.1\.1/ s/.*/127\.0\.1\.1\tPiHomie/g' /etc/hosts
+hostname -b $HOSTNAME
+sed -i -e '/127\.0\.1\.1/ s/.*/127\.0\.1\.1\t${HOSTNAME}/g' /etc/hosts
 
 # -- Apache2
 a2enmod wsgi
-sed -i -e "s|%%PIHOMIE_ROOT%%|${SCRIPT_PATH}|" $SCRIPT_PATH/apache_vhost.conf
-sed -i -e "s|%%PIHOMIE_GATEWAY%%|${SCRIPT_PATH}/gateway.wsgi|" $SCRIPT_PATH/apache_vhost.conf
-mv $SCRIPT_PATH/apache_vhost.conf /etc/apache2/sites-available/pihomie.conf
+sed -i -e "s|%%PIHOMIE_ROOT%%|${SCRIPT_PATH}|" $SCRIPT_PATH/res/apache_vhost.conf
+sed -i -e "s|%%PIHOMIE_GATEWAY%%|${ROOT_PATH}/controller/gateway.wsgi|" $SCRIPT_PATH/res/apache_vhost.conf
+mv $SCRIPT_PATH/res/apache_vhost.conf /etc/apache2/sites-available/pihomie.conf
 a2ensite pihomie
 service apache2 reload
 
