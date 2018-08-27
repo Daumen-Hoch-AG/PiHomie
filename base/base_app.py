552b4169b9df1dc9a0b48ea135a40cf579417dc8
Pitastic <mickep1985@gmail.com>
2018-08-27 22:07:08 +0200

    Logging auskommentiert

--- base/base_app.py
+++ base/base_app.py
@@ -21,7 +21,7 @@ def create_app(Host, config_path):
 	app.config['BaseDir'] = os.path.dirname(os.path.realpath(__file__))
 	app.config['FILE'] = config
 
-"""
+	"""
 	infoHandler = logging.FileHandler(app.config['FILE']['logging']['info'])
 	infoHandler.setLevel(logging.INFO)
 	app.logger.addHandler(infoHandler)
@@ -32,7 +32,7 @@ def create_app(Host, config_path):
 	errHandler = logging.FileHandler(app.config['FILE']['logging']['error'])
 	errHandler.setLevel(logging.ERROR)
 	app.logger.addHandler(errHandler)
-"""
+	"""
 
 	host = Host()
 
